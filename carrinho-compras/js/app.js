95ee0d90f2f3fa3a8dc22c2163bac1adc538fb7d
w7-silva <102619551+w7-silva@users.noreply.github.com>
2025-02-06 21:01:59 -0300

    Add files via upload

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,36 @@
+let total = 0;
+limpar();
+
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+
+    //alert(nomeProduto);
+    //alert(valorUnitario);
+    //alert(quantidade.value);
+
+    //calcular o preço, o nosso subtotal
+    let preco = quantidade * valorUnitario;
+    //adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
+
+    //Atualizar valor total
+    total = total + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${total}`;
+    document.getElementById('quantidade').value = 0;
+
+}
+
+function limpar() {
+    total = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+}
